f3a8d64ef10e9456ea55b61c1b044c163544ede7
CuongLH <lehuucuong270603@gmail.com>
2024-10-09 21:12:08 +0700

    update ui

--- src/pages/new/PaymentSuccess/index.js
+++ src/pages/new/PaymentSuccess/index.js
@@ -1,7 +1,7 @@
 import { Box, Button, Divider, Grid } from '@mui/material';
 import SuccessCircle from '~/components/new/SuccessCircle';
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import storageService from '~/components/StorageService/storageService';
 import AccountAPI from '~/API/AccountAPI';
 import SubscriptionAPI from '~/API/SubscriptionAPI';
@@ -11,23 +11,19 @@ export default function PaymentSuccess() {
 
     const [userInfo, setUserInfo] = useState(storageService.getItem('userInfo') || null);
 
-    useEffect(() => {
-        fetchUser();
-    }, []);
-
     const fetchUser = async () => {
         // This useEffect is now only for updating userInfo if it changes in localStorage
         const storedUserInfo = await storageService.getItem('userInfo');
 
         if (storedUserInfo !== null) {
-            // Fetch the updated user info based on the stored user id
-            const userResponse = await AccountAPI.getUserById(storedUserInfo?.id);
-            const currentPlan = await SubscriptionAPI.getByUserId(userInfo.id);
-            userResponse.planType = currentPlan.planType;
-            userResponse.subscriptionId = currentPlan.id;
-
             // Remove old userInfo from local storage
             storageService.removeItem('userInfo');
+
+            // Fetch the updated user info based on the stored user id
+            const userResponse = await AccountAPI.getUserById(storedUserInfo?.id);
+            const currentPlan = await SubscriptionAPI.getByUserId(userResponse?.id);
+            userResponse.planType = currentPlan.planType;
+            userResponse.subscriptionId = currentPlan.id;
 
             // Set the new user info in local storage
             storageService.setItem('userInfo', userResponse);
@@ -98,6 +94,7 @@ export default function PaymentSuccess() {
                             },
                         }}
                         onClick={() => {
+                            fetchUser();
                             navigate('/');
                         }}
                     >
